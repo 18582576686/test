dc82881312b6718e713626830181c1eb3b11b50c
18582576686 <973542623@qq.com>
2019-10-22 09:53:51 +0800

    ddddd Summary:ddddd
    Test Plan: dddd
    
    Reviewers: jiang.liu
    
    Reviewed By: jiang.liu
    
    Subscribers: jiang.liu
    
    Differential Revision: http://p.mydomain.com/D2

--- aaaaa.java
+++ aaaaa.java
@@ -3,4 +3,12 @@ class xxxx{
 	private int b;
 	private int c;
 	private int d;
+	private int a;
+	private int b;
+	private int c;
+	private int d;
+	private int a;
+	private int b;
+	private int c;
+	private int d;		
 }
